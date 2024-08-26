0d07ecd786252af045c329c7f79f00e61ee5d96b
Zain Haidar <86505295+zainhaidar16@users.noreply.github.com>
2024-08-26 14:32:26 +0000

    Fixed Minor Errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,39 @@
 import streamlit as st
 from sklearn.datasets import load_wine, load_breast_cancer, load_iris
-from  sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import warnings
+
+import matplotlib.pyplot as plt
+
 warnings.filterwarnings("ignore", category=FutureWarning)
-st.set_option('deprecation.showPyplotGlobalUse', False)
+
 def getClassifier(classifier):
     if classifier == 'SVM':
-        c = st.sidebar.slider(label='Choose value of C' , min_value=0.0001, max_value=10.0)
+        c = st.sidebar.slider(label='Choose value of C', min_value=0.0001, max_value=10.0)
         model = SVC(C=c)
     elif classifier == 'KNN':
-        neighbors = st.sidebar.slider(label='Choose Number of Neighbors',min_value=1,max_value=20)
-        model = KNeighborsClassifier(n_neighbors = neighbors)
+        neighbors = st.sidebar.slider(label='Choose Number of Neighbors', min_value=1, max_value=20)
+        model = KNeighborsClassifier(n_neighbors=neighbors)
     else:
         max_depth = st.sidebar.slider('max_depth', 2, 10)
         n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-        model = RandomForestClassifier(max_depth = max_depth , n_estimators= n_estimators,random_state= 1)
+        model = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=1)
     return model
-
 
 def getPCA(df):
     pca = PCA(n_components=3)
-    result = pca.fit_transform(df.loc[:,df.columns != 'Type'])
+    result = pca.fit_transform(df.loc[:, df.columns != 'Type'])
     df['pca-1'] = result[:, 0]
     df['pca-2'] = result[:, 1]
     df['pca-3'] = result[:, 2]
     return df
+
 def return_data(dataset):
     if dataset == 'Wine':
         data = load_wine()
@@ -39,10 +41,10 @@ def return_data(dataset):
         data = load_iris()
     else:
         data = load_breast_cancer()
-    df = pd.DataFrame(data.data, columns=data.feature_names , index=None)
+    df = pd.DataFrame(data.data, columns=data.feature_names, index=None)
     df['Type'] = data.target
     X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, random_state=1, test_size=0.2)
-    return X_train, X_test, y_train, y_test,df,data.target_names
+    return X_train, X_test, y_train, y_test, df, data.target_names
 
 # Title
 st.title("Classifiers in Action")
@@ -50,22 +52,21 @@ st.title("Classifiers in Action")
 # Description
 st.text("Choose a Dataset and a Classifier in the sidebar. Input your values and get a prediction")
 
-#sidebar
+# Sidebar
 sideBar = st.sidebar
-dataset = sideBar.selectbox('Which Dataset do you want to use?',('Wine' , 'Breast Cancer' , 'Iris'))
-classifier = sideBar.selectbox('Which Classifier do you want to use?',('SVM' , 'KNN' , 'Random Forest'))
+dataset = sideBar.selectbox('Which Dataset do you want to use?', ('Wine', 'Breast Cancer', 'Iris'))
+classifier = sideBar.selectbox('Which Classifier do you want to use?', ('SVM', 'KNN', 'Random Forest'))
 
-
-X_train, X_test, y_train, y_test, df , classes= return_data(dataset)
-st.dataframe(df.sample(n = 5 , random_state = 1))
+X_train, X_test, y_train, y_test, df, classes = return_data(dataset)
+st.dataframe(df.sample(n=5, random_state=1))
 st.subheader("Classes")
 
 for idx, value in enumerate(classes):
-    st.text('{}: {}'.format(idx , value))
+    st.text('{}: {}'.format(idx, value))
 
 # 2-D PCA
 df = getPCA(df)
-fig = plt.figure(figsize=(16,10))
+fig = plt.figure(figsize=(16, 10))
 sns.scatterplot(
     x="pca-1", y="pca-2",
     hue="Type",
@@ -78,8 +79,8 @@ plt.ylabel('PCA Two')
 plt.title("2-D PCA Visualization")
 st.pyplot(fig)
 
-#3-D PCA
-fig2 = plt.figure(figsize=(16,10)).gca(projection='3d')
+# 3-D PCA
+fig2 = plt.figure(figsize=(16, 10)).add_subplot(111, projection='3d')
 fig2.scatter(
     xs=df["pca-1"],
     ys=df["pca-2"],
