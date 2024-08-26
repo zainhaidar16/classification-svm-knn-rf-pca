2ce87ab4078f5c7fbbea62e452edc12b59141ba5
Zain Haidar <86505295+zainhaidar16@users.noreply.github.com>
2024-08-26 14:26:18 +0000

    Plotting PCA

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,17 +8,28 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-# Title
-st.title("Multi-Model Classification: SVM, KNN, Random Forest, and PCA")
 
-# Description
-st.text("Choose a Dataset and a Classifier in the sidebar. Input your values and get a prediction.")
+def getClassifier(classifier):
+    if classifier == 'SVM':
+        c = st.sidebar.slider(label='Choose value of C' , min_value=0.0001, max_value=10.0)
+        model = SVC(C=c)
+    elif classifier == 'KNN':
+        neighbors = st.sidebar.slider(label='Choose Number of Neighbors',min_value=1,max_value=20)
+        model = KNeighborsClassifier(n_neighbors = neighbors)
+    else:
+        max_depth = st.sidebar.slider('max_depth', 2, 10)
+        n_estimators = st.sidebar.slider('n_estimators', 1, 100)
+        model = RandomForestClassifier(max_depth = max_depth , n_estimators= n_estimators,random_state= 1)
+    return model
 
-#sidebar
-sideBar = st.sidebar
-dataset = sideBar.selectbox('Which Dataset do you want to use?',('Wine' , 'Breast Cancer' , 'Iris'))
-classifier = sideBar.selectbox('Which Classifier do you want to use?',('SVM' , 'KNN' , 'Random Forest'))
 
+def getPCA(df):
+    pca = PCA(n_components=3)
+    result = pca.fit_transform(df.loc[:,df.columns != 'Type'])
+    df['pca-1'] = result[:, 0]
+    df['pca-2'] = result[:, 1]
+    df['pca-3'] = result[:, 2]
+    return df
 def return_data(dataset):
     if dataset == 'Wine':
         data = load_wine()
@@ -31,9 +42,53 @@ def return_data(dataset):
     X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, random_state=1, test_size=0.2)
     return X_train, X_test, y_train, y_test,df,data.target_names
 
+
+# Title
+st.title("Classifiers in Action")
+
+# Description
+st.text("Choose a Dataset and a Classifier in the sidebar. Input your values and get a prediction")
+
+#sidebar
+sideBar = st.sidebar
+dataset = sideBar.selectbox('Which Dataset do you want to use?',('Wine' , 'Breast Cancer' , 'Iris'))
+classifier = sideBar.selectbox('Which Classifier do you want to use?',('SVM' , 'KNN' , 'Random Forest'))
+
+
 X_train, X_test, y_train, y_test, df , classes= return_data(dataset)
 st.dataframe(df.sample(n = 5 , random_state = 1))
 st.subheader("Classes")
 
 for idx, value in enumerate(classes):
     st.text('{}: {}'.format(idx , value))
+
+# 2-D PCA
+df = getPCA(df)
+fig = plt.figure(figsize=(16,10))
+sns.scatterplot(
+    x="pca-1", y="pca-2",
+    hue="Type",
+    palette=sns.color_palette("hls", len(classes)),
+    data=df,
+    legend="full"
+)
+plt.xlabel('PCA One')
+plt.ylabel('PCA Two')
+plt.title("2-D PCA Visualization")
+st.pyplot(fig)
+
+#3-D PCA
+fig2 = plt.figure(figsize=(16,10)).gca(projection='3d')
+fig2.scatter(
+    xs=df["pca-1"],
+    ys=df["pca-2"],
+    zs=df["pca-3"],
+    c=df["Type"],
+)
+fig2.set_xlabel('pca-one')
+fig2.set_ylabel('pca-two')
+fig2.set_zlabel('pca-three')
+plt.title("3-D PCA Visualization")
+st.pyplot(fig2.get_figure())
+
+
