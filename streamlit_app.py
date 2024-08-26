55859dbbf5cc80642e1bd9de680be87feccb55ad
Zain Haidar <86505295+zainhaidar16@users.noreply.github.com>
2024-08-26 14:25:20 +0000

    Initial Commit with Import Statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,39 @@
 import streamlit as st
+from sklearn.datasets import load_wine, load_breast_cancer, load_iris
+from  sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+# Title
+st.title("Multi-Model Classification: SVM, KNN, Random Forest, and PCA")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Description
+st.text("Choose a Dataset and a Classifier in the sidebar. Input your values and get a prediction.")
+
+#sidebar
+sideBar = st.sidebar
+dataset = sideBar.selectbox('Which Dataset do you want to use?',('Wine' , 'Breast Cancer' , 'Iris'))
+classifier = sideBar.selectbox('Which Classifier do you want to use?',('SVM' , 'KNN' , 'Random Forest'))
+
+def return_data(dataset):
+    if dataset == 'Wine':
+        data = load_wine()
+    elif dataset == 'Iris':
+        data = load_iris()
+    else:
+        data = load_breast_cancer()
+    df = pd.DataFrame(data.data, columns=data.feature_names , index=None)
+    df['Type'] = data.target
+    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, random_state=1, test_size=0.2)
+    return X_train, X_test, y_train, y_test,df,data.target_names
+
+X_train, X_test, y_train, y_test, df , classes= return_data(dataset)
+st.dataframe(df.sample(n = 5 , random_state = 1))
+st.subheader("Classes")
+
+for idx, value in enumerate(classes):
+    st.text('{}: {}'.format(idx , value))
